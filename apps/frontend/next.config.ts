dbd951998674d89962e6fac219b7239b6ab3c60f
FedericoPaz <=>
2026-08-14 22:16:32 -0300

    fix: minors

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
-  allowedDevOrigins: ["proplanta-sistema.ar"],
+  allowedDevOrigins: ['sistemademo.cabecitanegra.dpdns.org'],
   async headers() {
     return [
       {
